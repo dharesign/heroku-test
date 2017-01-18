69a63ff0de0e6b8651bbdefe7c68321a79c446f5
Daryl Haresign <dharesign@bloomberg.net>
2017-01-18 12:59:41 -0500

    Fix typo

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require("http");
 
 http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    repsonse.end("Hello World\n");
+    response.end("Hello World\n");
 }).listen(process.env.PORT);
