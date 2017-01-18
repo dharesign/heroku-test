116f6ca9ad2bbddc70fd83200f9b5da28cc82f7e
Daryl Haresign <dharesign@bloomberg.net>
2017-01-18 12:58:40 -0500

    Test

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const http = require("http");
+var http = require("http");
 
-var server = http.createServer(function (request, response) {
+http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
     repsonse.end("Hello World\n");
-});
-
-server.listen(process.env.PORT);
+}).listen(process.env.PORT);
